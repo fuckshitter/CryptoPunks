1868f7a8e61ec4f61fdb3932b90106430813e8a8
Armen Merikyan <armenmerik@gmail.com>
2021-08-12 15:13:18 -0700

    changine the name to askweedman punks

--- src/components/FormAndPreview/FormAndPreview.jsx
+++ src/components/FormAndPreview/FormAndPreview.jsx
@@ -87,7 +87,7 @@ class FormAndPreview extends Component {
                   className="form-control"
                   placeholder="Enter Punk Id"
                   onChange={(e) =>
-                    this.setState({ punkid: e.target.value })
+                    {this.setState({ punkid: e.target.value }), this.Load_New_Image}
                   }
                 />
               </div>
