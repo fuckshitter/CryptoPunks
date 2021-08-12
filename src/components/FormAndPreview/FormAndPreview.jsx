5f6d1f0f9a4528d5e68e886c3d6638320259fcef
Armen Merikyan <armenmerik@gmail.com>
2021-08-12 12:58:04 -0700

    adding code to view image

--- src/components/FormAndPreview/FormAndPreview.jsx
+++ src/components/FormAndPreview/FormAndPreview.jsx
@@ -56,13 +56,14 @@ class FormAndPreview extends Component {
         <img
             source = {{ uri: this.state.cryptoBunkImageURL }}
             />
+       <form onSubmit={this.Load_New_Image} className="pt-4 mt-1">
         <button
-          onClick={this.Load_New_Image}
           id="mintBtn22"
           style={{ fontSize: "0.9rem", letterSpacing: "0.14rem" }}
           type="submit"
           className="btn mt-4 btn-block btn-outline-primary"
         >
+      </form>
         Load Punk
         </button>
         <form onSubmit={this.callClaimPunkFromApp} className="pt-4 mt-1">
