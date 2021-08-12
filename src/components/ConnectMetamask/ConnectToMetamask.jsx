b39216ad8eb7bac8926a416a9f84694a5cbacc08
Armen Merikyan <armenmerik@gmail.com>
2021-08-12 15:06:23 -0700

    changine the name to askweedman punks

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -9,7 +9,10 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
       </h1>
       <p className="lead">
         This is an NFT marketplace where you can claim your Baked Punks.
-        
+      </p>
+      <hr className="my-4" />
+      <p className="lead">
+          Contract Address : 0xead759ec13b02c21ae87044f009ce97c1c72371f
       </p>
       <hr className="my-4" />
       <button
