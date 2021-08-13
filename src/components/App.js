868e5d46bc23d249b266af77d47cd7c53a803cd9
Armen Merikyan <armenmerik@gmail.com>
2021-08-12 20:39:35 -0700

    changine the name to askweedman punks

--- src/components/App.js
+++ src/components/App.js
@@ -119,8 +119,8 @@ class App extends Component {
 
 
 
-          const totalTokensOwnedByAccount2 = cryptoBoysContract.numberOfPunksReserved;
-          this.state.totalTokensOwnedByAccount  = totalTokensOwnedByAccount2 +11;
+          const totalTokensOwnedByAccount2 = cryptoBoysContract.totalSupply;
+          this.state.totalTokensOwnedByAccount  = totalTokensOwnedByAccount2;
           this.setState({totalTokensOwnedByAccount:this.state.totalTokensOwnedByAccount});
 
   	       this.setState({ loading: false });
