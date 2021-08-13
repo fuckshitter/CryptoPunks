09d2a36d1dcb4bbdac333fbbf8da4ef5f5f9de9a
Armen Merikyan <armenmerik@gmail.com>
2021-08-13 11:14:13 -0700

    changine the name to askweedman punks

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -12,8 +12,18 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
       </p>
       <hr className="my-4" />
       <p className="lead">
+      What is a BakedPunk?
+      BakedPunk is an unique one of a kind Non-Fungible Token (NFT) on Binance.
+      By minting a BakedPunk, you become the proud owner of the very first punk NFT on the Binance network.
+      What’s more is, there can only ever be 10,000 punks.
+      When you decide to mint one, you will receive a unique-one-of-a-kind
+      BakedPunk with proof of ownership stored on the polygon network. Who knows? 
+      Your very first BakedPunk NFT may be worth your first house or car!
+      </p>
+      <p className="lead">
           Contract Address : 0xead759ec13b02c21ae87044f009ce97c1c72371f
       </p>
+
       <hr className="my-4" />
       <button
         onClick={connectToMetamask}
