e0a4eb72c59535164d3d8ae4ace2f662d06e7ace
Armen Merikyan <armenmerik@gmail.com>
2021-08-13 13:33:07 -0700

    changine the name to askweedman punks

--- src/components/MyCryptoBoys/MyCryptoBoys.jsx
+++ src/components/MyCryptoBoys/MyCryptoBoys.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import CryptoBoyNFTImage from "../CryptoBoyNFTImage/CryptoBoyNFTImage";
 import MyCryptoBoyNFTDetails from "../MyCryptoBoyNFTDetails/MyCryptoBoyNFTDetails";
 import Loading from "../Loading/Loading";
+import ReactPaginate from 'react-paginate';
 
 const MyCryptoBoys = ({
   accountAddress,
