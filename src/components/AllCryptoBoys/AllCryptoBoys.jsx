74a1e37af0cffc64a36521cd81e738e722f69dc2
Armen Merikyan <armenmerik@gmail.com>
2021-08-13 13:49:28 -0700

    changine the name to askweedman punks

--- src/components/AllCryptoBoys/AllCryptoBoys.jsx
+++ src/components/AllCryptoBoys/AllCryptoBoys.jsx
@@ -1,4 +1,4 @@
-import React, { Component }, { useState } from "react";
+import React, { Component, useState } from "react";
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 class AllCryptoBoys extends Component {
