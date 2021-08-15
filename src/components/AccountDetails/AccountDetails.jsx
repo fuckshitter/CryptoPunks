5c1fb8e354a9f116fa7e03ea79baa101d332e493
Armen Merikyan <armenmerik@gmail.com>
2021-08-14 18:21:04 -0700

    changine the name to askweedman punks

--- src/components/AccountDetails/AccountDetails.jsx
+++ src/components/AccountDetails/AccountDetails.jsx
@@ -9,18 +9,21 @@ const AccountDetails = ({ accountAddress, accountBalance }) => {
           This is an NFT marketplace where you can get BakedPunks.
         </p>
         <hr className="my-4" />
-        <div class="card col-md-3" >
+        <div class="card col-md-12" >
           <div class="card-body">
             <p className="lead">
                 Contract Address : 0xead759ec13b02c21ae87044f009ce97c1c72371f
             </p>
           </div>
         </div>
-        <hr className="my-4" />
-        <p className="lead">Account address :</p>
-        <h4>{accountAddress}</h4>
-        <p className="lead">Account balance :</p>
-        <h4>{accountBalance} BNB</h4>
+        <div class="card col-md-12" >
+          <div class="card-body">
+            <p className="lead">Account address :</p>
+            <h4>{accountAddress}</h4>
+            <p className="lead">Account balance :</p>
+            <h4>{accountBalance} BNB</h4>
+          </div>
+        </div>
       </div>
     </div>
   );
