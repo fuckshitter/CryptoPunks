35d806f6192feffb5cecb22009d7d6abcd2eb1a8
Armen Merikyan <armenmerik@gmail.com>
2021-08-15 10:13:15 -0700

    changine the name to askweedman punks

--- src/components/FormAndPreview/FormAndPreview.jsx
+++ src/components/FormAndPreview/FormAndPreview.jsx
@@ -165,21 +165,6 @@ class FormAndPreview extends Component {
         <form onSubmit={this.callMintMyNFTFromApp} className="pt-4 mt-1">
           <div className="row">
             <div className="col-md-12">
-              <div>
-                <label htmlFor="punkId">Punk ID</label>
-                <input
-                  required
-                  type="number"
-                  name="punkid"
-                  id="punkid"
-                  value={this.state.punkid}
-                  className="form-control"
-                  placeholder="Enter Punk Id"
-                  onChange={(e) =>
-                      this.Load_New_Image(e)
-                  }
-                />
-              </div>
 	      <div>
                 <label htmlFor="price">Price</label>
                 <input
