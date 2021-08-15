d7eca58c50ff8031a3b3b86497d22cef9cd925f3
Armen Merikyan <armenmerik@gmail.com>
2021-08-15 09:06:34 -0700

    changine the name to askweedman punks

--- src/components/FormAndPreview/FormAndPreview.jsx
+++ src/components/FormAndPreview/FormAndPreview.jsx
@@ -117,7 +117,7 @@ class FormAndPreview extends Component {
                     <div class="card-body">
                 <img
                     src={this.state.cryptoBunkImageURL}
-                    class="img-thumbnail"
+                    class="img-thumbnail card-img-top"
                     />
     <h5 class="card-title">Punk NO {this.state.punkid}</h5>
     <h6 class="card-subtitle mb-2 text-muted">Price {this.state.cryptoBoyPrice} BNB</h6>
