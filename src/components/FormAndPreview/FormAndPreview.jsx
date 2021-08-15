653df21c2d2796f060f7ac8c5c87ca42e75ed71b
Armen Merikyan <armenmerik@gmail.com>
2021-08-15 10:19:30 -0700

    changine the name to askweedman punks

--- src/components/FormAndPreview/FormAndPreview.jsx
+++ src/components/FormAndPreview/FormAndPreview.jsx
@@ -126,7 +126,7 @@ class FormAndPreview extends Component {
                               />
                               <hr className="my-4" />
     <h5 class="card-title">Punk NO {this.state.punkid}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Price {this.state.cryptoBoyPrice} BNB</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Price <b>{this.state.cryptoBoyPrice} BNB</b></h6>
     <p class="card-text">OWNER : {this.state.punkOwner}</p>
 
                     </div>
