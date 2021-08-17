5ea8c25fb50e5852d5258e93993f4192c7ae2c5a
Armen Merikyan <armenmerik@gmail.com>
2021-08-17 15:51:49 -0700

    adding punks for sale page

--- src/components/PunksForSale/PunksForSale.jsx
+++ src/components/PunksForSale/PunksForSale.jsx
@@ -50,7 +50,7 @@ class PunksForSale extends Component {
       while (s.length < 4) s = "0" + s;
       var newImageUrl = '/images/punks/punk-' + s + '.png';
       var newLinkUrl = '/mint?punkid=' + index;
-      items.push(<div class="card col-md-2" ><img src={newImageUrl} /><div class="card-body"> <h5 class="card-title">NO {index}</h5><p class="card-text"> PRICE {value}</p><Link to={newLinkUrl} className="nav-link" >Select</Link></div></div>)
+      items.push(<div class="card col-md-2" ><img src={newImageUrl} /><div class="card-body"> <h5 class="card-title">NO {index}</h5><p class="card-text"> PRICE {value} BNB</p><Link to={newLinkUrl} className="nav-link" >BUY</Link></div></div>)
 //      items.push(<Link to={newLinkUrl} className="nav-link" ><img src={newImageUrl} /></Link>)
     }
 
