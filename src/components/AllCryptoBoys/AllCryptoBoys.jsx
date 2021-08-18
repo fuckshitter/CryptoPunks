a3f5b0fa0dfc9f68b0ca038adffae6bbb8c8aac0
Armen Merikyan <armenmerik@gmail.com>
2021-08-18 13:17:35 -0700

    adding pagination

--- src/components/AllCryptoBoys/AllCryptoBoys.jsx
+++ src/components/AllCryptoBoys/AllCryptoBoys.jsx
@@ -69,7 +69,7 @@ class AllCryptoBoys extends Component {
     const numberofPages = this.props.cryptoBoys.length/500;
         for (let j=0;j<numberofPages;j++) {
           var newLinkUrl = 'loadPage';
-          pages[j] = <li class="page-item"><button onclick={this.loadPage} className="nav-link" value={j} >{j}</button></li>;
+          pages[j] = <li class="page-item"><button onclick={this.loadPage} value={j} >{j}</button></li>;
         }
     return (
       <div>
