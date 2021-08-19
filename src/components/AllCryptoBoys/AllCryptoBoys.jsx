e773c9f3a2afe0b34b46666bc720bc7d3de34b6b
Armen Merikyan <armenmerik@gmail.com>
2021-08-18 18:54:41 -0700

    adding pagination

--- src/components/AllCryptoBoys/AllCryptoBoys.jsx
+++ src/components/AllCryptoBoys/AllCryptoBoys.jsx
@@ -80,7 +80,7 @@ class AllCryptoBoys extends Component {
       <p className="lead">
         View All 10,000 BakedPunks. Current Page {this.state.currentPage} Punks Loaded {this.props.cryptoBoys.length}
         <nav aria-label="Page navigation example">
-          <ul class="pagination">
+          <ul class="pagination pagination-sm">
             {pages}
           </ul>
         </nav>
--- src/components/ContractNotDeployed/ContractNotDeployed.jsx
+++ src/components/ContractNotDeployed/ContractNotDeployed.jsx
@@ -6,7 +6,9 @@ const ContractNotDeployed = () => {
       <h3>BakedPunks Contract Not Deployed To This Network.</h3>
       <hr className="my-4" />
       <p className="lead">
-        Connect Metamask to Binance running a custom RPC
+        Please Connect Metamask to Binance Smart Chain Network. 
+        <a href="https://academy.binance.com/en/articles/connecting-metamask-to-binance-smart-chain" >Connecting MetaMask to Binance Smart Chain</a>
+
       </p>
     </div>
   );
