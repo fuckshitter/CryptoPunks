461af3b9a465490ff7bfef0f56ac035dd92a5a60
Armen Merikyan <armenmerik@gmail.com>
2021-08-22 19:30:40 -0700

    adding pagination

--- src/components/ContractNotDeployed/ContractNotDeployed.jsx
+++ src/components/ContractNotDeployed/ContractNotDeployed.jsx
@@ -6,7 +6,7 @@ const ContractNotDeployed = () => {
       <h3>BakedPunks Contract Not Deployed To This Network.</h3>
       <hr className="my-4" />
       <p className="lead">
-        Please Connect Metamask to Binance Smart Chain Network. 
+        Please Connect Metamask to Binance Smart Chain Network.  
         <a href="https://academy.binance.com/en/articles/connecting-metamask-to-binance-smart-chain" >Connecting MetaMask to Binance Smart Chain</a>
 
       </p>
