11a277f72990af0b2c4472116c93d1a93b8855db
Armen Merikyan <armenmerik@gmail.com>
2021-08-23 08:01:02 -0700

    adding v2 or punk detail

--- src/components/PunksForSale/PunksForSale.jsx
+++ src/components/PunksForSale/PunksForSale.jsx
@@ -70,7 +70,7 @@ class PunksForSale extends Component {
         while (s.length < 4) s = "0" + s;
           var newImageUrl = "/images/punks/punk-" + s + "x8.png";
           var newLinkUrl = "/buypunk?punkid=" + value.index;
-          return ( <div class="card col-md-3"><Link to={newLinkUrl} className="nav-link"><img class="img-thumbnail" src={newImageUrl} id={value.index} onMouseEnter={this.callViewPunkDetail} onMouseOver={this.callViewPunkDetailOff} /></Link>  <div class="card-body"> {" "} <h5 class="card-title">NO {value.index}</h5> <p class="card-text"> PRICE {value.price} BNB</p> <Link to={newLinkUrl} className="nav-link"> BUY </Link> </div> </div> );
+          return ( <div class="card col-md-3"><Link to={newLinkUrl} className="nav-link"><img class="img-thumbnail" src={newImageUrl} id={value.index} onMouseEnter={this.callViewPunkDetail} onMouseLeave={this.callViewPunkDetailOff} /></Link>  <div class="card-body"> {" "} <h5 class="card-title">NO {value.index}</h5> <p class="card-text"> PRICE {value.price} BNB</p> <Link to={newLinkUrl} className="nav-link"> BUY </Link> </div> </div> );
       }
     });
 
