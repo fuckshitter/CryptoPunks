949474ee848f712f133692a9dbb1a8f52faa614c
Armen Merikyan <armenmerik@gmail.com>
2021-08-25 14:07:26 -0700

    adding v2 or punk detail

--- src/components/BuyPunk/BuyPunk.jsx
+++ src/components/BuyPunk/BuyPunk.jsx
@@ -130,7 +130,7 @@ class BuyPunk extends Component {
     return (
       <div>
         <div class="card col-md-12 text-center" >
-                    <div class="card-body">
+                    <div class="card-body bg-info">
                           <img
                               src={this.state.cryptoBunkImageURL}
                               class="img-thumbnail  col-md-6"
